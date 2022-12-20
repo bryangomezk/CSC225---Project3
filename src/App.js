00009e2a3e011e27734719d3d544d2522438876b
Bryan Gomez <bryangr711@gmail.com>
2022-12-19 21:03:52 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
+import Employee from './Components/Employee';
+import Employees from './Components/Employees';
+import Loading from './Components/Loading';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const[loading, setLoading] = useState(true);
+const[employeeData, setEmployeeData] = useState ([]);
+const[selectedEmployee, setSelectedEmployee] = useState(null);
+const resetState = () => {
+  setEmployeeData([]);
+  setSelectedEmployee(null);
+  setLoading(true);
+  getEmployeeData();
+}
+
+const getEmployeeData = (id = null) => {
+  setLoading(true);
+
+  let actualId = ' ';
+  if(!!id && parseInt(id, 10) > 0) {
+    actualId = parseInt(id,10)
+  }
+
+  axios.get(`https://api.matgargano.com/employees/${actualId}`).then(response => {
+  setEmployeeData(response.data);
+  setLoading(false);
+
+
+  })
+}
+
+useEffect (() => {
+      getEmployeeData();
+}, []);
+
+useEffect(() => {
+
+  if(!!selectedEmployee){
+    getEmployeeData(selectedEmployee);
+  }
+
+}, [selectedEmployee])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {!!loading && <Loading />}
+      {!loading && <div> 
+        
+      {/* list of Employees */}
+      {!selectedEmployee && <Employees setSelectedEmployee={setSelectedEmployee} data={employeeData} />}
+
+      {/* Individual List of Employees */}
+      {!!selectedEmployee && <Employee resetState={resetState} data = {employeeData} />}
+
+      </div>}
+
+
     </div>
   );
 }
